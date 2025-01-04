884306fd3e7922656ad2dc7d1c88003a05c416b1
SathviktheG <sathvikgowda199@gmail.com>
2025-01-05 00:01:36 +0530

    Update posts.jsx

--- client/src/reducers/posts.jsx
+++ client/src/reducers/posts.jsx
@@ -1,29 +1,3 @@
-// export default(posts=[],action)=>{
-//     switch (action.type) {
-//         case 'FETCH_ALL':
-//             return posts;
-//         case 'CREATE':
-//             return posts;
-//         default:
-//             return posts;
-//     }
-// }
-// const initialState = []; // Default initial state
-
-// const postsReducer = (posts = initialState, action) => {
-//   switch (action.type) {
-//     case'DELETE':
-//       return posts.filter((post)=>post._id != action.payload);
-//     case'UPDATE':
-//       return posts.map((post)=>post._id=== action.payload._id ? action.payload : post);
-//     case 'FETCH_ALL':
-//       return action.payload; // Assuming action.payload contains the fetched posts
-//     case 'CREATE':
-//       return [...posts, action.payload]; // Assuming action.payload contains the new post
-//     default:
-//       return posts;
-//   }
-// };
 import { FETCH_ALL, CREATE, UPDATE, DELETE,LIKE } from '../constants/actionTypes';
 
 export default (posts = [], action) => {
